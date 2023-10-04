d936856377dd5cf7de40d00d4423983faae82f9a
IllusoryedoC <codeoffantasy@gmail.com>
2023-10-04 16:52:03 +0200

    Creating Links

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,8 +1,12 @@
 'use client';
 
 import { motion } from 'framer-motion';
+import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
+import { HiDownload } from 'react-icons/hi';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+import { FaGithubSquare } from 'react-icons/fa';
 
 const Intro = () => {
   return (
@@ -52,10 +56,60 @@ const Intro = () => {
         <span className="font-bold">Hello, I&apos;m Ihar.</span> I&apos;m a{' '}
         <span className="font-bold">full-stack developer</span> with{' '}
         <span className="font-bold">1 year</span> of experience. I enjoy
-        building
-        <span className="italic">sites & apps</span>. My focus is{' '}
-        <span className="underline">React (Next.js)</span>.
+        building <span className="italic">sites & apps</span>. My focus is{' '}
+        <span className="underline ">React (Next.js)</span>.
       </motion.p>
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full 
+          outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105
+          transition
+          "
+        >
+          Contact me here
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+
+        <a
+          className="group bg-white  px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 
+          hover:scale-110 active:scale-105
+          transition cursor-pointer border border-black/10"
+          href="/CV.pdf"
+          download
+        >
+          Download CV{' '}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] 
+          hover:scale-[1.15] active:scale-105 hover:text-gray-950
+          transition cursor-pointer border border-black/10"
+          href="https://www.linkedin.com/in/ihar-bahamaz-0120111aa/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.35rem] focus:scale-[1.15] 
+          hover:scale-[1.15] hover:text-gray-950 active:scale-105
+          transition cursor-pointer border border-black/10"
+          href="https://github.com/IllusoryedoC"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 };
