c1166bba389035f6998e5600fd7283d4da1a7210
IllusoryedoC <codeoffantasy@gmail.com>
2023-10-07 00:34:23 +0200

    Creating Contact

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,21 +1,49 @@
+'use client';
 import React from 'react';
 import SectionHeading from './SectionHeading';
 import { FaPaperPlane } from 'react-icons/fa';
+import { motion } from 'framer-motion';
+import useSectionInView from '@/hooks/useSectionInView';
 
 const Contact = () => {
+  const { ref } = useSectionInView('Contact');
+
   return (
-    <section id="contact" className="mb-20 sm:mb-28 w-[min(100%,38rem)]">
+    <motion.section
+      ref={ref}
+      id="contact"
+      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1,
+      }}
+      viewport={{
+        once: true,
+      }}
+    >
       <SectionHeading>Contact me</SectionHeading>
-      <p className="text-gray-700">
+      <p className="text-gray-700 -mt-6">
         Please contact me directly at{' '}
         <a className="underline" href="mailto:example@gmail.com">
           codeoffantasy@gmail.com
         </a>{' '}
         or through this form.
       </p>
-      <form className="mt-10 flex flex-col ">
-        <input type="email" className="h-14 rounded-lg borderBlack" />
-        <textarea className="h-52 mv-3 rounded-lg borderBlack p-4" />
+      <form className="mt-10 flex flex-col">
+        <input
+          type="email"
+          className="h-14 px-4 rounded-lg borderBlack"
+          placeholder="Your email"
+        />
+        <textarea
+          className="h-52 my-3 rounded-lg borderBlack p-4"
+          placeholder="Your message"
+        />
         <button
           type="submit"
           className="group flex items-center justify-center  h-[3rem] gap-2 bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 
@@ -25,7 +53,7 @@ const Contact = () => {
           <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:translate-y-1" />
         </button>
       </form>
-    </section>
+    </motion.section>
   );
 };
 
