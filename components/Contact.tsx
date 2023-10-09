7d2ac64daeca58111ba5531d4863fe1bf5043c99
IllusoryedoC <codeoffantasy@gmail.com>
2023-10-09 19:57:50 +0200

    Before deployment

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -29,7 +29,7 @@ const Contact = () => {
       }}
     >
       <SectionHeading>Contact me</SectionHeading>
-      <p className="text-gray-700 -mt-6">
+      <p className="text-gray-700 -mt-6 dark:text-light-white/80">
         Please contact me directly at{' '}
         <a className="underline" href="mailto:example@gmail.com">
           codeoffantasy@gmail.com
@@ -54,12 +54,12 @@ const Contact = () => {
           type="email"
           required
           maxLength={500}
-          className="h-14 px-4 rounded-lg borderBlack"
+          className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
           placeholder="Your email"
         />
         <textarea
           name="message"
-          className="h-52 my-3 rounded-lg borderBlack p-4"
+          className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
           placeholder="Your message"
           required
           maxLength={5000}
