e54ce0b9b42bffb9ec70c28172afe9758ab4c85d
IllusoryedoC <codeoffantasy@gmail.com>
2023-11-02 20:36:31 +0100

    Changing Intro

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -17,11 +17,11 @@ const Intro = () => {
   return (
     <section
       ref={ref}
-      id="home"
-      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+      id='home'
+      className='mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]'
     >
-      <div className="flex items-center justify-center">
-        <div className="relative">
+      <div className='flex items-center justify-center'>
+        <div className='relative'>
           <motion.div
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
@@ -31,17 +31,17 @@ const Intro = () => {
             }}
           >
             <Image
-              src="/IMG_0549.jpg"
-              alt="person"
-              width="192"
-              height="192"
-              quality="95"
+              src='/IMG_0549.jpg'
+              alt='person'
+              width='192'
+              height='192'
+              quality='95'
               priority={true}
-              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+              className='h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl'
             />
           </motion.div>
           <motion.span
-            className="absolute bottom-0 right-0 text-4xl "
+            className='absolute bottom-0 right-0 text-4xl '
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{
@@ -58,19 +58,21 @@ const Intro = () => {
       </div>
 
       <motion.h1
-        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I&apos;m Ihar.</span> I&apos;m a{' '}
-        <span className="font-bold">full-stack developer</span> with{' '}
-        <span className="font-bold">1 year</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{' '}
-        <span className="underline ">React (Next.js)</span>.
+        <span className='font-bold'>Hello & Welcome! I&apos;m Ihar</span> a
+        highly motivated <span className='font-bold'>full-stack developer</span>{' '}
+        with a year of valuable experience.{' '}
+        <span className='font-bold'>Building </span>websites & apps is not just
+        a job for me, <span className='italic'>it&apos;s a passion</span>. My
+        expertise centers on React & <span className='underline '>Next.js</span>
+        .
       </motion.h1>
 
       <motion.div
-        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{
@@ -78,47 +80,47 @@ const Intro = () => {
         }}
       >
         <Link
-          href="#contact"
-          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full 
+          href='#contact'
+          className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full 
           outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105
           transition
-          "
+          '
           onClick={() => {
             setActiveSection('Contact');
             setTimeOfLastClick(Date.now());
           }}
         >
           Contact me here{' '}
-          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+          <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition' />
         </Link>
 
         <a
-          className="group bg-white  px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 
+          className='group bg-white  px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 
           hover:scale-110 active:scale-105
-          transition cursor-pointer border borderBlack dark:bg-white/10"
-          href="/CV.pdf"
+          transition cursor-pointer border borderBlack dark:bg-white/10'
+          href='/IgorBogomazCV.pdf'
           download
         >
           Download CV{' '}
-          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+          <HiDownload className='opacity-60 group-hover:translate-y-1 transition' />
         </a>
 
         <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] 
+          className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] 
           hover:scale-[1.15] active:scale-105 hover:text-gray-950
-          transition cursor-pointer border borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://www.linkedin.com/in/ihar-bahamaz-0120111aa/"
-          target="_blank"
+          transition cursor-pointer border borderBlack dark:bg-white/10 dark:text-white/60'
+          href='https://www.linkedin.com/in/iharbahamaz/'
+          target='_blank'
         >
           <BsLinkedin />
         </a>
 
         <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.35rem] focus:scale-[1.15] 
+          className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.35rem] focus:scale-[1.15] 
           hover:scale-[1.15] hover:text-gray-950 active:scale-105
-          transition cursor-pointer border borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com/IllusoryedoC"
-          target="_blank"
+          transition cursor-pointer border borderBlack dark:bg-white/10 dark:text-white/60'
+          href='https://github.com/IharBahamaz'
+          target='_blank'
         >
           <FaGithubSquare />
         </a>
